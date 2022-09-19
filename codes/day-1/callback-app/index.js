999dd70251e38e10c9c71ed8388c71f8760cf8ed
Joydip <joydip.mondal@gmail.com>
2022-09-19 13:41:26 +0530

    Update index.js

--- codes/day-1/callback-app/index.js
+++ codes/day-1/callback-app/index.js
@@ -3,7 +3,7 @@ function filterValues(arr) {
 }
 
 var numbers = [1, 3, 2, 4, 5, 7, 6, 0, 9, 8]
-var filteredAray = filterValues(numbers)
-for (var i = 0; i < filterValues.length; i++) {
-    console.log(filterValues[i])
+var filteredArray = filterValues(numbers)
+for (var i = 0; i < filteredArray.length; i++) {
+    console.log(filteredArray[i])
 }
