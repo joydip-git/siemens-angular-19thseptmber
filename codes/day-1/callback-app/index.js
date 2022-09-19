3e4ea30e6348889267f1932fe2a8992e9c6db4da
Joydip <joydip.mondal@gmail.com>
2022-09-19 13:40:47 +0530

    callback app uploaded

---
+++ codes/day-1/callback-app/index.js
@@ -0,0 +1,9 @@
+function filterValues(arr) {
+
+}
+
+var numbers = [1, 3, 2, 4, 5, 7, 6, 0, 9, 8]
+var filteredAray = filterValues(numbers)
+for (var i = 0; i < filterValues.length; i++) {
+    console.log(filterValues[i])
+}
